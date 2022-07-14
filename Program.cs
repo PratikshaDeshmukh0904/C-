87059380ce61cf04db221b6aaceba3296aa8862e
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-14 19:57:10 +0530

    ArgumentButNoReturnValue

--- Program.cs
+++ Program.cs
@@ -4,31 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Add
+namespace ArgumentButNoReturnValue
 {
     class Program
     {
-        int num1;
-        int num2;
-        int result;
-
-        public void addition()
-        {
-            result = num1 + num2;
-            Console.WriteLine(result);
-            Console.ReadLine();
-        }
-           
         static void Main(string[] args)
         {
-            Program obj = new Program();
-
-            obj.num1 = 10;
-            obj.num2 = 20;
-        
-        
-            obj.addition();
-
+            Program sum = new Program();
+            sum.sum();
+        }
+        void sum()
+        {
+            int a = 29;
+            int b = 45;
+            int add;
+            add = a + b;
+            Console.WriteLine(add);
+            Console.ReadLine();
         }
     }
 }
