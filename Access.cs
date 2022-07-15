e30ecf8ce901dd658b1171491162dd7d97470ab2
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-15 16:39:13 +0530

    AccessModifier

---
+++ Access.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifier
+{
+    class Access
+    {
+        
+    }
+    class car
+    {
+        private string model;
+        private string color;
+        private int price;
+
+        private void printinfo()
+        {
+            Console.WriteLine("Model:" + model);
+            model = Console.ReadLine();
+            Console.WriteLine("Color:" + color);
+            color = Console.ReadLine();
+            Console.WriteLine("Price:" + price);
+            price = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+            car obj = new car();
+            obj.printinfo();            //To run program add main method in same class 
+        }
+    }
+}
