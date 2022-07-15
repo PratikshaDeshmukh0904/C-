22eb55c6ec7b4e5ec4d1b256e4951488da6a3e24
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-15 16:41:20 +0530

    ArgumentAndReturnValue

---
+++ ArgymentReturnValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgymentAndReturnValue
+{
+    class ArgymentReturnValue
+    {
+        static void Main(string[] args)
+        {
+            ArgymentReturnValue Obj = new ArgymentReturnValue();
+            Obj.sum(10,20);
+        }
+        int sum(int num1 , int num2)
+        {
+            int result;
+           
+            result = num1 + num2;
+            return result;
+        }
+    }
+}
