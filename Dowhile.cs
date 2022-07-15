e696ca843bd00328393d17e38762332c4f78e7f8
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-15 16:42:03 +0530

    doWhile.cs

---
+++ Dowhile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhile
+{
+    class Dowhile
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            do
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            while (a < 10);
+            Console.ReadLine();
+            
+        }
+    }
+}
