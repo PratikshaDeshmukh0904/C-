fa09ef5fb2f5312d93e0454286d1fc0a2d807d4f
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-15 16:43:37 +0530

    ifelse.cs

---
+++ Ifelse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ifelse
+{
+    class Ifelse
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            if(a<b)
+            {
+                Console.WriteLine("b", b);
+            }
+            else
+            {
+                Console.WriteLine("a", a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
