e13a4116ec53684d8ac6543883b5fe53f5797398
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-15 16:45:03 +0530

    NoArgumentButReturnValue

---
+++ NoArgumentButReturnValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoArgumentButReturnValue
+{
+    class NoArgumentButReturnValue
+    {
+        static void Main(string[] args)
+        {
+
+            NoArgumentButReturnValue sum = new NoArgumentButReturnValue();
+            sum.Sum();
+            Console.WriteLine(sum.Sum());
+            Console.ReadLine();
+        }
+
+        int Sum()
+        {
+            int Add;
+            int a = 10;
+            int b = 20;
+            Add = a + b;
+            return Add;
+        }
+        
+       
+
+    }
+}
