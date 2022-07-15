6328c9f134ea8700d3b30ff25a93eaade357b57a
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-15 16:44:14 +0530

    ifelseladder.cs

---
+++ ifElseladder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifElseladder
+{
+    class ifElseladder
+    {
+        static void Main(string[] args)
+        {
+            int marks = 40;
+
+            if (80 <= marks)
+            {
+                Console.WriteLine("Distinction");
+            }
+            else if (70 <= marks)
+            {
+                Console.WriteLine("First Class");
+            }
+            else if (50 <= marks)
+            {
+                Console.WriteLine("Second Class");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
