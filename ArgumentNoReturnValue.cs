b833f4618c210a621393d79fa987e7f1740e3f6c
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-15 16:40:18 +0530

    ArgumentNoReturnValue

---
+++ ArgumentNoReturnValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgumentButNoReturnValue
+{
+    class ArgumentNoReturnValue
+    {
+        static void Main(string[] args)
+        {
+            ArgumentNoReturnValue sum = new ArgumentNoReturnValue();
+            sum.sum();
+        }
+        void sum()
+        {
+            int a = 29;
+            int b = 45;
+            int add;
+            add = a + b;
+            Console.WriteLine(add);
+            Console.ReadLine();
+        }
+    }
+}
