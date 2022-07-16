542721c741c5328258657a78654d185667a27d9b
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-16 07:55:47 +0530

    NoArgumentNoReturnValue

---
+++ NoArgumentNoReturnValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoArgumentNoReturnValue
+{
+    class NoArgumentNoReturnValue
+    {
+
+        void argument()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            NoArgumentNoReturnValue a = new NoArgumentNoReturnValue();
+            a.argument();
+        }
+    }
+}
