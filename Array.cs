956e43708d2ecc4eb1c90254b2704ce4565d934b
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-17 14:24:31 +0530

    Array

---
+++ Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            string car1 = "Maruti";
+            string car2 = "BMW";
+            string car3 = "Ford";
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);          
+            Console.WriteLine(car3);        //to avoid these lengthy code use array
+            Console.ReadLine();
+        }
+    }
+}
