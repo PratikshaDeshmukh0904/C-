5efcd142d1303bf265c1d64576e6ef0c09403585
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-17 14:26:05 +0530

    break

---
+++ break.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @break
+{
+    class @break
+    {
+        static void Main(string[] args)
+        {
+            for(int b=15 ; b<=20 ;b--)
+            {
+                Console.WriteLine(b);
+                if(b==10)
+                {
+                    break;
+                }
+                Console.ReadLine();
+            }
+
+             
+        }
+
+        
+            
+    }
+}
