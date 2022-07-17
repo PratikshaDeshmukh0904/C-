9906f39bcf54e5dc4f88ec2e279362b73c813964
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-18 00:37:23 +0530

    Add files via upload

---
+++ Constructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Constructor
+    {
+        public Constructor()  //constructor have same class name,and constructor have return type
+        {
+            Console.WriteLine("Constructor");
+        }
+        static void Main(string[] args)
+        {
+            Constructor obj = new Constructor();
+            Console.ReadLine();
+        }
+    }
+}
