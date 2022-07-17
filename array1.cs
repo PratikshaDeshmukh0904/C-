c0b6f9aa212724c87c11dbc77ababdc15ebf4f65
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-17 14:25:07 +0530

    array1

---
+++ array1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array1
+{
+    class array1
+    {
+        static void Main(string[] args)
+        {
+
+            string[] cars = new string[5] {"Maruti","Ford","BMW","Ford1","Swift"};
+            Console.WriteLine(cars[0]);
+            Console.WriteLine(cars[1]);
+            Console.WriteLine(cars[2]);
+            Console.WriteLine(cars[3]);
+            Console.WriteLine( cars[4]);
+            Console.ReadLine();
+            
+        }
+    }
+}
