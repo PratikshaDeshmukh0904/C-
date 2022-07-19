2d24d2a6acc45f490c62ca526d805ba195822b10
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-19 13:05:25 +0530

    Continue

---
+++ Continue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue
+{
+    class Continue
+    {
+        static void Main(string[] args)
+        {
+
+            
+            for(int a=10;a<=20;a--)
+            {
+                Console.WriteLine(a);
+                if(a==5)
+                {
+                    continue;//it force to loop to go next iteration
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
