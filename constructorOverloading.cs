847eedf6e24336e8b7de122aa25f02a3203baa0f
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-19 13:04:45 +0530

    ConstructorOverloading

---
+++ constructorOverloading.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorOverloading
+{
+    class constructorOverloading
+    {
+
+        
+    }
+    class car
+    {
+        string carModel;
+        int carYear;
+        public car(string Model,int Year)
+        {
+             carModel = Model;
+             carYear = Year;
+
+        }
+        public car(int Year,string Model)
+        {
+             carModel = Model;
+            carYear = Year;
+
+        }
+        static void Main(string[] args)
+        {
+            car myCar = new car("BMW", 2022);
+            Console.WriteLine("Model:"+myCar.carModel);
+            Console.WriteLine("Year:" + myCar.carYear);
+            Console.ReadLine();
+        }
+    }
+
+}   
+
