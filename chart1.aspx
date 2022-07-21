0a7da76a227ef1f045f1d676b9bd20728e123c45
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-21 11:51:12 +0530

    chart control in asp.net

---
+++ chart1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="chart1.aspx.cs" Inherits="chart1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="form2" runat="server">
+        <div>
+            <asp:Chart ID="Chart2" runat="server" Width="800px" Height="600px">            
+                 <Legends>  
+                    <asp:Legend Alignment="Center" Docking="Bottom" IsTextAutoFit="False" Name="Default"  
+                        LegendStyle="Row" />  
+                </Legends>  
+                <Series>
+                    <asp:Series Name="series" ChartType="Column" ></asp:Series>
+                   <%-- <asp:Series Name="Humidity" ChartType="Column"></asp:Series>
+                    <asp:Series Name="WindSpeed" ChartType="Column"></asp:Series>
+                    <asp:Series Name="Winddirection" ChartType="Column"></asp:Series>
+                    <asp:Series Name="SolarRadiation" ChartType="Column"></asp:Series>
+                    <asp:Series Name="RainfalTodayTotal" ChartType="Column"></asp:Series>
+                   --%>
+                    
+                </Series>
+                <ChartAreas>
+                    <asp:ChartArea Name="ChartArea1" BorderWidth="0" >
+                        
+                        <AxisY Title="AirTemperature(AVG)"></AxisY>
+                    </asp:ChartArea>
+                </ChartAreas>
+            </asp:Chart>
+        </div>
+    </form>
+  
+</body>
+</html>
---
+++ chart1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Web.UI.DataVisualization.Charting;
+
+public partial class chart1 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            GetChartData();
+            //GetChartType();
+        }
+
+    }
+    private void GetChartData()
+    {
+        string json = (new WebClient()).DownloadString("http://late.aashayweather.com/api/weather/Data.php?StationName=cwprs");
+        var obj = JsonConvert.DeserializeObject<DataTable>(json);
+
+        
+        
+        //string Timestamp = obj.Rows[0][0].ToString();
+        //string AmbTemperature = obj.Rows[0][1].ToString();
+        //string Humidity = obj.Rows[0][2].ToString();
+        //string WindSpeed = obj.Rows[0][3].ToString();
+        //string Winddirection = obj.Rows[0][4].ToString();
+        //string SolarRadiation = obj.Rows[0][5].ToString();
+        //string RainfalTodayTotal = obj.Rows[0][6].ToString();
+
+        Series series = Chart2.Series["series"];
+        series.Points.AddXY("AmbTemperature", obj.Rows[0][1].ToString());
+
+        //Series series2 = Chart2.Series["series"];
+        series.Points.AddXY("Humidity", obj.Rows[0][2].ToString());
+       // Series series3 = Chart2.Series["series"];
+        series.Points.AddXY("WindSpeed", obj.Rows[0][3].ToString());
+       // Series series4 = Chart2.Series["series"];
+        series.Points.AddXY("Winddirection", obj.Rows[0][4].ToString());
+        //Series series5 = Chart2.Series["series"];
+        series.Points.AddXY("SolarRadiation", obj.Rows[0][5].ToString());
+        //Series series6 = Chart2.Series["series"];
+        series.Points.AddXY("RainfalTodayTotal", obj.Rows[0][6].ToString());
+
+        {
+
+        }
+    }
+}
