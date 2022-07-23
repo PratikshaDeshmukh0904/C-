8c5971907f40516daf30504670071b7366f52960
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-23 20:04:31 +0530

    Encapsulation

---
+++ Encapsulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Encapsulation
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+            int c= myAccount.accountBalance = 100;
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+    class Account
+    {
+        public int accountBalance = 10000;
+    }
+}
