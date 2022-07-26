a173e9e992e1d761a0eeacec0933a5c6cb726584
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-26 12:40:24 +0530

    Method1

---
+++ method1.cs
@@ -0,0 +1,31 @@
+﻿using System;  //Default Class
+
+
+namespace method1 //namespace
+{
+    class method1 //class
+    {
+        
+        
+        public int Findmax(int num1,int num2)//pass parameters
+        {
+            int result;
+            if(num1>num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+
+            return result;
+        }
+        static void Main(string[] args) //default method
+        {
+            method1 obj = new method1();
+            obj.Findmax(10,20);   //pass arguments
+
+        }
+    }
+}
