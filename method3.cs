2a88edda1ee9b670cbed374f44d8f98ce2744c6c
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-26 12:58:26 +0530

    method3

---
+++ method3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method3
+{
+    class method3
+    {
+        
+        static void Main(string[] args)
+        {
+            
+
+            int no1 = 10;
+            int no2 = 20;
+            method3 maxno = new method3();
+             int c=maxno.Findmax(no1,no2);
+            Console.WriteLine( c);
+            Console.ReadLine();
+           
+        }
+
+        public int Findmax(int no1,int no2)
+        {
+            int result;
+           
+            if(no1>no2)
+            {
+                result = no1;
+            }
+            else
+            {
+                result = no2;
+            }
+            return result;
+            
+        }
+    }
+}
