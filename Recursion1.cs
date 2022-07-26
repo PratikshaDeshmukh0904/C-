9a170672de6ca9364c752599a26e36fc67627256
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-26 12:38:57 +0530

    Recursion

---
+++ Recursion1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Recursion1
+{
+    class Recursion1
+    {
+        int num = 1;
+        void counting()
+        {
+            if(num == 30)
+            {
+                return; //In Recursion Method must have an exit condition
+            }
+            Console.WriteLine(num);
+            num++;
+            counting();//recursion means method can call itself again & again
+        }
+        static void Main(string[] args)
+        {
+            Recursion1 count = new Recursion1();
+            count.counting();
+            Console.ReadLine();//Process is terminated due to StackOverflowException.
+           
+
+        }
+    }
+}
