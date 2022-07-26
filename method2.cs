c1f0f83ba8b6f438efa3d0e783f28df2c62657fd
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-26 12:42:55 +0530

    Method2

---
+++ method2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method2
+{
+    class method2
+    {
+        public int max(int a, int b)
+        {
+            int result;
+            if(a>b)
+                {
+                result = a;
+                }
+            else
+            {
+                result = b;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            method2 max = new method2();
+            max.max(a,b);
+            Console.WriteLine(max.max(a, b));
+            Console.ReadLine();
+        }
+    }
+}
