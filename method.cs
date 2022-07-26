7b046bb35fb239ea7ec3c4558acc1fd912e0b2cd
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-26 12:39:51 +0530

    Method

---
+++ method.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    class method
+    {
+        int a = 10;
+        int b = 20;
+        int result;
+
+        void add()
+        {
+            result = a + b;
+            displayresult();
+
+        }
+
+        void displayresult()
+        {
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            method obj = new method();
+            obj.add();
+
+        }
+    }
+}
