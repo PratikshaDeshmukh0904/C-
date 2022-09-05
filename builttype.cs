511eba4e693e2c48e5d286093c03c4363a3f0392
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-09-05 15:16:43 +0530

    builttype

---
+++ builttype.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace builttype
+{
+    class builttype
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            int i = 0;
+            Console.WriteLine("Min = {0} ", int.MinValue);
+            Console.WriteLine("Max ={0}", int.MaxValue);
+
+
+            string name = "Pratiksha";
+            Console.WriteLine( name);
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
