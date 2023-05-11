a89e88cca05c1c9d847288e08d60f4778febd57b
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-10 21:00:38 -0700

    Add files via upload

---
+++ Method/method.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class method
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Pratiksha",22);
+            SayHi("Aarti",21);
+            SayHi("PD",23);
+            Console.ReadLine();
+        }
+        static void SayHi( string name,int age)
+        {
+            Console.WriteLine("Hello User "+name+" your age is " +age);
+        }
+    }
+}
