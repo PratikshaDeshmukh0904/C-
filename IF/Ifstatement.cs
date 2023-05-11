6989fde6cb6f56c8273e0f1b67ad1a2564870168
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-10 20:58:48 -0700

    If Statement

---
+++ IF/Ifstatement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF
+{
+    internal class Ifstatement
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+            bool isTall = false;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are tall male");
+            }
+            else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are short male");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("You are either not male or not tall or both");
+            }
+            Console.ReadLine();
+        }
+    }
+}
