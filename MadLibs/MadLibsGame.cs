b4e31342b6587056272e7db4d68fe8ff0d3d1ad7
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-10 20:59:43 -0700

    Mad Libs Game

---
+++ MadLibs/MadLibsGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibs
+{
+    internal class MadLibsGame
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.WriteLine("Enter a color: ");
+            color= Console.ReadLine();
+
+            Console.WriteLine("Enter a Plural ");
+            pluralNoun= Console.ReadLine();
+
+            Console.WriteLine("Enter a celebrity");
+            celebrity= Console.ReadLine();
+
+            Console.WriteLine("Roses are "+color);
+            Console.WriteLine(pluralNoun +"are blue");
+            Console.WriteLine("I Love "+celebrity);
+            Console.ReadLine();
+        }
+    }
+}
