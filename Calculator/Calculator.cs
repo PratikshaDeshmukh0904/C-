23c3d7756ecd9e8e6cf0d6af9f3c62587fd942ba
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-10 20:55:44 -0700

    Calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num+6);
+
+            Console.WriteLine("Enter a Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Second Number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1+ num2);
+            Console.ReadLine();
+
+        }
+    }
+}
