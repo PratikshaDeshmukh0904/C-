5395af788097ea8f0270a802c56fdc3681dcf6b0
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-10 21:01:45 -0700

    Nested If

---
+++ MoreIf/Moreif.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreIf
+{
+    internal class Moreif
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(10, 20,70));
+            Console.ReadLine(); 
+        }
+        static int GetMax(int num1,int num2,int num3)
+        {
+            int result;
+            if (num1>=num2 && num1>=num3)
+            {
+                result= num1;
+            }
+            else if(num2>=num1 && num2>=num3)
+            {
+                result= num2;
+            }
+            else
+            {
+                result= num3;
+            }
+            return result;
+        }
+    }
+}
