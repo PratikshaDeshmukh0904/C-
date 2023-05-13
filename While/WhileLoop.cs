80dd579bb85ffbabe37e21bb3667fc755b465289
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-13 04:13:19 -0700

    Add files via upload

---
+++ While/WhileLoop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    internal class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.WriteLine("Do-While");
+            index = 1;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            while (index <= 5);
+            Console.ReadLine();
+        }
+    }
+}
