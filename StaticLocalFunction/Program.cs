1ee6c78e6afec0d2df8d63c065809b421a98cc6d
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-17 22:24:14 -0700

    Static Local Function

---
+++ StaticLocalFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿class Student
+{
+    public void DisplyaMArks(int m1, int m2, int m3)
+    {
+        double avgMarks = getAverageMarks(m1,m2,m3);
+        System.Console.WriteLine("Marks1: " + m1);
+        System.Console.WriteLine("Marks2: " + m2);
+        System.Console.WriteLine("Marks3: " + m3);
+        System.Console.WriteLine("Average MArks: " + avgMarks);
+
+        //create local functions
+       static double getAverageMarks(int mark1,int mark2,int mark3)
+        {
+            //create local variable of local function
+            double avg;
+            avg = (double)(mark1 + mark2 + mark3) / 3;
+            return avg;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //create object for Student Class
+        Student s = new Student();
+
+        //call displaymarks
+        s.DisplyaMArks(45, 67, 89);
+
+        System.Console.ReadKey();
+    }
+}
