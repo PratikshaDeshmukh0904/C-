6e8c97f906888b428538c7284053830bc78016b5
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-19 01:59:44 -0700

    Properties

---
+++ Properties/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //create three onjects for employee
+        Employee emp1 = new Employee(); //constructor3
+        emp1.EmpID = 10;
+        emp1.EmployeeName = "Pratik";
+        emp1.Job = "Manager";
+        emp1.Tax = 1000;
+        emp1.NativePlace = "New Delhi";
+        Employee emp2 = new Employee(102, "allen"); //constructor 2
+        Employee emp3 = new Employee(103, "Anna", "Clerk"); //constructor 1
+        Employee emp4 = new Employee() { EmployeeName = "For", Job = "Executive" };
+
+        //display fields
+        System.Console.WriteLine("CompanyName", Employee.CompanyName);
+        System.Console.WriteLine(emp1.EmpID);
+        System.Console.WriteLine(emp2.EmployeeName);
+        System.Console.WriteLine(emp1.Job);
+        System.Console.WriteLine(emp1.Salary);
+        System.Console.WriteLine(emp1.CalculateNetSalary());
+        System.Console.WriteLine(emp2.NativePlace);
+        System.Console.ReadKey();
+    }
+}
---
+++ PropertyLibrary/Class1.cs
@@ -0,0 +1,136 @@
+﻿public class Employee
+{
+    //instance field
+    private int _empId;
+    private string _empName;
+    private string _job;
+
+    private double _salary;
+
+    private double _tax;
+    //instance property
+    public int EmpID
+    {
+        set 
+        {
+           if(value >=100)
+            {
+                _empId = value;
+            }
+        }
+        get
+        {
+            return _empId;
+        }
+    }
+
+    //instance property
+    public string EmployeeName
+    {
+        set
+        {
+            if (value.Length >= 100)
+            {
+                _empName = value;
+            }
+        }
+        get
+        {
+            return _empName;
+        }
+    }
+
+    //instance property
+    public string Job
+    {
+        set
+        {
+            _job = value;
+        }
+        get
+        {
+            return _job;
+        }
+    }
+    //static field
+    private static string _companyName;
+
+    //static property
+    public static string CompanyName
+    {
+        set
+        {
+            if(value.Length >= 20)
+            {
+                _companyName = value;
+            }
+        }
+        get
+        {
+            return _companyName;
+        }
+    }
+    //instance constructor
+    public Employee(int empId,string empName,string job)
+    {
+       this._empId = empId;
+       this._empName = empName;
+       this._job = job;
+       this._salary = 1000;
+    }
+
+    //constructor2
+    public Employee(int empId,string empName)
+    {
+        _empId = empId;
+        _empName = empName;
+        this._salary = 1000;
+
+    }
+    //Constructor 3
+    public Employee()
+    {
+        _empId = 1;
+        this._salary = 1000;
+    }
+    //static constructor
+    static Employee()
+    {
+        _companyName = "ABC industries";
+    }
+
+    //readonly property
+    public double Salary
+    {
+        get
+        {
+            return _salary;
+            
+        }
+    }
+
+    //writeonly
+    public double Tax
+    {
+        set
+        {
+            if(value >= 0 && value<=100 )
+            {
+                _tax = value;
+            }
+           
+        }
+    }
+
+    //method
+    public double CalculateNetSalary()
+    {
+        double t = Salary - _tax;
+        return t;
+    }
+
+    //automatic properties
+    public string NativePalce { get; set; } = "New York";
+
+
+}
