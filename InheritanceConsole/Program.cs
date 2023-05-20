e9580c53850b9093713a4be6d4200bc705625573
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-19 22:01:08 -0700

    Inheritance

--- InheritanceConsole/Program.cs
+++ InheritanceConsole/Program.cs
@@ -15,6 +15,8 @@ namespace InheritanceConsole
             emp1.EmployeeID = 101;
             emp1.EmployeeName = "Scott";
             emp1.Location = "Sangli";
+            Employee emp2 = new Employee(103,"P","k");
+
             System.Console.WriteLine("Object of Parent Class Employee:");
             System.Console.WriteLine(emp1.EmployeeID);
             System.Console.WriteLine(emp1.Location);
@@ -27,11 +29,16 @@ namespace InheritanceConsole
             mgr1.EmployeeName = "Scott";
             mgr1.Location = "Hyderabad";
             mgr1.DepartmentName = "Accounting";
+            Manager mgr2 = new Manager(104,"a","n","Accounting");
             System.Console.WriteLine("Object of child class manager");
             System.Console.WriteLine (mgr1.EmployeeID);
             System.Console.WriteLine(mgr1.Location);
             System.Console.WriteLine(mgr1.EmployeeName);
             System.Console.WriteLine(mgr1.DepartmentName);
+            System.Console.WriteLine(mgr1.GetFullDepartmentName());
+            System.Console.WriteLine(mgr1.GetHealthInsuranceMethod());//it will execute child class method o/p will be 1500
+            System.Console.WriteLine(mgr1.GetHealthInsuranceMethod1());//it execute parent class method 1st and then child class
+
             System.Console.WriteLine();
 
 
@@ -41,6 +48,7 @@ namespace InheritanceConsole
             s1.EmployeeName = "Scott";
             s1.Location = "Hyderabad";
             s1.Region = "Karad";
+            SalesMan s2 = new SalesMan(12, "g", "g", "j");
             System.Console.WriteLine("Object of child class SalesMan");
             System.Console.WriteLine(s1.EmployeeID);
             System.Console.WriteLine(s1.Location);
@@ -50,3 +58,32 @@ namespace InheritanceConsole
         }
     }
 }
+//Method Hiding 
+/*It is concept which is used to hide(override) the parent class's method,by creating another method in the child class with
+ * same name and same parameters
+ * 
+ * 
+ *Creating Parent class
+ 
+ class ParentClassName
+ {
+   public void MethodName(param1,...)
+   {
+   }
+ }
+
+*Creating Child class
+
+class ChildClassName:ParentClassName
+{
+    public new void MethodName(param1,...)
+    {
+    }
+}
+
+when method hiding is done,if the method is called using child class's object,the child 
+class's method only executes ;parent class's method will not be executed.
+
+method hiding is done automatically; but is recommended to use "new" keyword (but not must)
+ *
+ */
