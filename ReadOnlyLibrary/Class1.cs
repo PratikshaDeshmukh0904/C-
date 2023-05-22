c6204f6eb04e010d2df2a454282cb1b01bfcf2fd
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-22 02:22:55 -0700

    Read Only Structure

---
+++ ReadOnlyLibrary/Class1.cs
@@ -0,0 +1,33 @@
+﻿using ReadOnlyLibrary;
+namespace ReadOnlyLibrary
+{
+    public class Class1
+    {
+
+    }
+    public readonly struct Marvel
+    {
+        //field
+        private readonly string _characterName;
+
+        //public readonly property
+        public string CharacterName
+        { 
+            get 
+            {
+                return _characterName;
+            }
+        }
+        //public method
+        public void PrintCharacterName()
+        {
+            System.Console.WriteLine(this.CharacterName);
+            
+        }
+        //constructor
+        public Marvel(string characterName)
+        {
+            this._characterName = characterName;
+        }
+    }
+}
---
+++ ReadOnlyStructureExample/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReadOnlyLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        //create structure instance
+        Marvel m=new Marvel("Thanos");
+
+        //invoke the properties and methods
+        System.Console.WriteLine(m.CharacterName);
+        m.PrintCharacterName();
+        System.Console.ReadKey();
+    }
+}
