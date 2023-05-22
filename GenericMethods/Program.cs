da6683b098fe0bb9fad2f82e1310e4cd57282993
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-22 05:58:26 -0700

    Generic Methods

---
+++ GenericMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //create object
+        Sample sample = new Sample();
+        Employee emp=new Employee() { Salary=1000};
+        Student student = new Student() { Marks = 80 };
+
+        //call the generic method
+        sample.PrintData<Employee>(emp);
+        sample.PrintData<Student>(student);
+        System.Console.ReadKey();
+    }
+}
+
+
+
+/*
+ * Generic Method is a method that has one or more generic parameters
+ * 
+ * You can restrict what type of data types(class names)allowed to be passed while creating object.
+ */
