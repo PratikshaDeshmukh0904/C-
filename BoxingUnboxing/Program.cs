a850295bd7bb227670143c0247797e9d1e4f3f88
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-22 03:52:19 -0700

    Boxing-Unboxing

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ Boxing =Conversion of value type to reference type
+
+ */
+class Program
+{
+    static void Main()
+    {
+        //primitive variable
+        int x = 10;
+
+        //boxig value type to refernce type
+        object obj = x;
+        System.Console.WriteLine(x);//10
+        System.Console.WriteLine(obj);//10
+
+        //refernce type into value type
+        object obj1 = 20;
+
+        //unboxing
+        int y =(int)obj;
+
+        System.Console.WriteLine(y);
+        System.Console.WriteLine(obj1);
+
+        System.Console.ReadKey();
+    }
+}
