cf316aa935f763cc55e670c9ae3d7f8f0713fc26
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-22 03:51:08 -0700

    Object Class

---
+++ ObjectClassExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ObjectClassLibrary;
+class Program
+{
+    static void Main()
+    {
+        //create an object of PErson class
+        System.Object obj = new Person() { PersonName ="Scott",Email="scott2@gmail.com"};
+        
+        //access methods
+        Console.WriteLine(obj.Equals(new Person()));//False
+                                                    
+        Console.WriteLine(obj.Equals(new Person() { PersonName = "Scott", Email = "scott2@gmail.com" }));
+        Console.WriteLine(obj.GetHashCode());
+        Console.WriteLine(obj.ToString());
+        Console.WriteLine(obj.GetType().ToString());
+        Console.ReadKey();
+    }
+}
+
+/*
+ 1.All C# classes,structures,interfaces,enumerations are children of System.Object class.
+2.Every method defined in the Object class is available in all objects in the system as all classes in .Net Framework are derived from Object class
+ 
+ 
+ */
---
+++ ObjectClassLibrary/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace ObjectClassLibrary
+{
+    public class Person 
+    {
+        //properties
+        public string PersonName { get; set; }
+        public string Email { get; set; }
+
+        //overriding equals method
+        public override bool Equals(object? obj)
+        {
+            Person p=(Person)obj;
+            if(this.PersonName==p.PersonName && this.Email == p.Email)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //overriding ToString method
+        public override string ToString()
+        {
+            return "Person name: " + this.PersonName;
+        }
+    }
+}
