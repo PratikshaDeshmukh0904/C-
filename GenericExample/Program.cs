9dbdcedb21dbf6652d45fef488ce8c8d245dbbf5
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-22 05:36:37 -0700

    Generics

---
+++ GenericExample/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //create object of generic class
+        User<int,int> user = new User<int,int>();
+        User<bool,string> user1 = new User<bool,string>();
+
+        //set value into generic field
+        user.Registrationstatus = 1234;
+        user1.Registrationstatus = false;
+
+        user.Age = 22;
+        user1.Age = "35-40";
+        System.Console.WriteLine(user.Registrationstatus);
+        System.Console.WriteLine(user1.Registrationstatus);
+
+        System.Console.WriteLine(user.Age);
+        System.Console.WriteLine(user1.Age);
+        System.Console.ReadKey();   
+
+    }
+}
+
+/*
+ *Generic class is a class,which contains one or more "type parameters".
+ *
+ class ClassName<T>
+ {
+    public T FieldName;
+ }
+*object of Generic class
+
+ClassName<int> referenceVariable = new ClassName<int>();
+ */
---
+++ GenericLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿//Generic Class
+public class User<T1,T2>
+{
+    //generic field
+    public T1 Registrationstatus;
+
+    //another generic field
+    public T2 Age;
+}
