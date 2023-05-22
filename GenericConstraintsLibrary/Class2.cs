a3c1f5112083f05a36e74e56d036baf41caa1cb2
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-22 05:37:41 -0700

    Generic Constraints

---
+++ GenericConstraintsLibrary/Class2.cs
@@ -0,0 +1,22 @@
+﻿public abstract class Student
+{
+    public abstract int Marks { get; set; }
+}
+public class GraduateStudent : Student
+{
+    public override int Marks { get; set; }
+}
+public class PostGraduation : Student
+{
+    public override int Marks { get; set; }
+}
+//generic class
+public class MarksPrinter<T> where T : Student
+{
+    public T stu;
+    public void PrintMarks()
+    {
+        Student temp=(Student)stu;
+        System.Console.WriteLine(temp.Marks);
+    }
+}
