6612a5e4266c295655a8c73f0884fd48862c469f
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-21 21:15:37 -0700

    Static class

---
+++ StaticClassExample/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ 1.static class is a class that can contain only "static members".
+ 
+ 2.if you don't want even single 'instance member'
+    then use 'static class'.
+
+ 3.we can avoid accidental creation of object for the class,by making it as "static class".
+
+ 
+ */
+
+class Program
+{
+    static void Main()
+    {
+        //access static field
+        Console.WriteLine(Country.CountryName);
+        Console.WriteLine(Country.NoOfState);
+        Console.WriteLine(Country.GetNoOfUnionTerritories());
+
+        Console.ReadKey();
+    }
+}
---
+++ StaticClassLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿public static class Country
+{
+    public static string CountryName = "india";
+    public static int NoOfState = 28;
+
+    public static int GetNoOfUnionTerritories()
+    {
+        return 8;
+    }
+}
