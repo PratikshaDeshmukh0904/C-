8fc9a67384ebed2787e66170cf37894bb4783c1a
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-23 04:44:17 -0700

    Dynamically Typed

---
+++ DynamicallyTyped/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DynamicallyTyped
+{
+    class Student
+    {
+        public string StudentName { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            //dynamically typed variable
+            dynamic x;
+            x = 100;
+            x = "Hello";
+            x = new Student() { StudentName="Harsha"};
+
+            Console.WriteLine(x.StudentName as String);
+            Console.ReadKey();
+
+        }
+    }
+}
