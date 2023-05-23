10276d9fc6c3967dac2c94e0b08a8c9626e0455e
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-23 07:14:38 -0700

    IDisposal

---
+++ IDisposal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using IDisposalLibrary;
+
+namespace IDisposal
+{
+    class Program
+    {
+        static void Work()
+        {
+            //create object using declaration
+            using Sample s = new Sample();
+            
+               s.DisplayDataFromDatabase();
+            
+        }//Dispose gets called here
+        static void Main()
+        {
+            Work();
+            //create object using "using structure"
+           /* using(Sample s= new Sample())
+            {
+                s.DisplayDataFromDatabase();
+            }*/
+            Console.WriteLine("Some other work here");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IDisposalLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace IDisposalLibrary
+{
+    public class Sample:System.IDisposable
+    {
+        //constructor
+        public Sample()
+        {
+            Console.WriteLine("Database Connected");
+        }
+
+        //method
+        public void DisplayDataFromDatabase()
+        {
+            Console.WriteLine("Reading data from database");
+        }
+
+        //Dispose
+        public void Dispose()
+        {
+            Console.WriteLine("Database disconnected");
+        }
+    }
+}
