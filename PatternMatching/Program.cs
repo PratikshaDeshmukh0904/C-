157fd72b28d11238c54e14357c3fae13425f26e3
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-23 03:21:19 -0700

    Pattern Matching

---
+++ PatternMatching/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ It allows you to declare a variable,while checking the data type(class)of a reference varaible
+ */
+
+using PatternMatchingLibrary;
+using System;
+
+namespace PatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //reference variable of parent class
+            ParentClass pc;
+
+            //object of child class
+            pc = new ChildClass() { x = 10, y = 20 };
+
+            //access parent class member
+            if(pc is ChildClass cc)
+            {
+                //ChildClass cc = (ChildClass)pc;
+                Console.WriteLine(cc.x);
+                Console.WriteLine(cc.y);
+            }
+           
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PatternMatchingLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace PatternMatchingLibrary
+{
+    //parent class
+    public class ParentClass
+    {
+        public int x { get; set; }
+    }
+    //child class
+    public class ChildClass : ParentClass
+    {
+        public int y { get; set; }
+    }
+}
