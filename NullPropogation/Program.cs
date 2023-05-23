5fd91b17608dc97893947c86e4fe250e8e50330a
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-23 00:42:33 -0700

    Null Propagation

---
+++ NullPropogation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NullPropogation
+{
+    class Person
+    {
+        public int Age;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            //p is null
+            Person p1=new Person() { Age=20};
+            //Console.WriteLine((p1==null)?null:Convert.ToString(p1.Age));
+           
+            Console.WriteLine(p1?.Age);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+/*
+ Null Propagation Operator (?.) and (?[]) checks the value of
+ left-hand operand whether it is null or not.
+
+referenceVariable?.fieldName
+
+--is same as  --
+
+(referenceVariable == null) ? null : referenceVaiable.fieldName;
+ 
+ */
