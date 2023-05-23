9056d3671e3f28d7b2b8cf32f0effa599621de20
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-23 04:42:40 -0700

    Implicitly Typed

---
+++ ImplicitlyTyped/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ The variable that are declared with 'var' keyword are called as 'implicitly-typed variables'
+var variableName=value;
+ 
+ */
+
+namespace ImplicitlyTyped
+{
+    class Program
+    {
+        static void Main()
+        {
+           // namespace1.namespace2.namespace3.Person p;
+           var p= new namespace1.namespace2.namespace3.Person() { PersonName = "Pratiksha" };
+
+            var p2="Harsha".ToUpper();
+
+            Console.WriteLine(p.PersonName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ImplicitlyTypedLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace namespace1
+{
+    namespace namespace2
+    {
+        namespace namespace3
+        {
+            public class Person
+            {
+                public string PersonName { get; set; }
+
+            }
+        }
+            
+    }
+
+   
+}
