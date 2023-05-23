6506f6e2b331c8f8230559e64d2e9bd3158f52ca
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-22 22:33:37 -0700

    Handling Null

---
+++ HandlingNull/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Person
+{
+    public int? NoOfChildren;
+}
+class Program
+{
+    static void Main()
+    {
+        //create object
+        Person p1=new Person() { NoOfChildren=2};
+        Person p2 = new Person() { NoOfChildren = null };
+        Person p3 = null;
+        //Console.WriteLine(p1.NoOfChildren);
+        //if(p2.NoOfChildren.HasValue)
+        //{
+            //int x = p2.NoOfChildren.Value;
+            Console.WriteLine(p2.NoOfChildren??0);
+        //}
+
+
+
+
+        System.Console.ReadKey();
+    }
+}
+/*
+ The 'null coalescing operator' checks whether the value is null or not.
+ It returns the left-hand-side operand if the value is not null.
+ It returns the right-hand-side operand if the value is null.
+
+Simplifying the syntax of 'if statement' to check if the value is nu
+**Null Coalescing Operator
+
+variableName??valueIfNull
+
+ 
+ */
