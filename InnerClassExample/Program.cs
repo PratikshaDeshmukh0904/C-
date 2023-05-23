bc1d67bf52a7072d505f1dad4e5d3bd8b7f280ba
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-23 07:12:18 -0700

    Inner Class

---
+++ InnerClassExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using InnerClassLibrary;
+class Program
+{
+    static void Main()
+    {
+        //create an object of inner class
+        InnerClassLibrary.MarksCalculation.CalculationHelper ch = new InnerClassLibrary.MarksCalculation.CalculationHelper();
+        
+        //call the inner class's method
+        Console.WriteLine(ch.Multiply(10, 5));
+
+        //call outer class's method
+        InnerClassLibrary.MarksCalculation mc = new InnerClassLibrary.MarksCalculation();
+        Student s = new Student() {SecuredMarks=35,MaxMarks=50};
+
+        mc.CalculatePercentage(s);
+        Console.WriteLine(s.Percentage);
+        Console.ReadKey();
+    }
+}
---
+++ InnerClassLibrary/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace InnerClassLibrary
+{
+    public class Student
+    {
+        public double SecuredMarks;
+        public double MaxMarks;
+        public double Percentage;
+    }
+    //outer class
+    public class MarksCalculation
+    {
+        public void CalculatePercentage(Student s)
+        {
+            //create object of inner class
+            CalculationHelper ch = new CalculationHelper();
+            //s.Percentage = s.SecuredMarks / s.MaxMarks * 100;
+            s.Percentage = s.SecuredMarks / ch.Multiply(s.MaxMarks,100);
+        }
+        //inner class
+       public class CalculationHelper
+        {
+            public double Multiply(double n1, double n2)
+            {
+                return n1 * n2;
+            }
+        }
+    }
+}
