671f60f2af4316c92bfd8be8dbb134366e38d6d7
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-05-29 00:46:16 -0700

    One To One Relation

---
+++ OneToOneExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using College;
+
+namespace OneToOneExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //student class's object
+            Student student = new Student();
+            student.RollNo = 1;
+            student.StudentName = "Scott";
+            student.Email = "Scott@gmail.com";
+            
+
+            //branch class's object
+            Branch branch = new Branch();
+            branch.BranchName = "Compute Science";
+            branch.NoOfSemesters = 8;
+
+            //one-to-one relation
+            student.branch = new Branch();
+            student.branch.BranchName = "Computer Science";
+            student.branch.NoOfSemesters = 8;
+
+            //display
+            Console.WriteLine(student.RollNo);
+            Console.WriteLine(student.StudentName);
+            Console.WriteLine(student.Email);
+            Console.WriteLine(student.branch.NoOfSemesters);
+            Console.WriteLine(student.branch.BranchName);
+
+            Console.ReadKey();
+        }
+    }
+}
